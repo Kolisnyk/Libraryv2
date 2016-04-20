de89af4156fd3d72ed922b06baa5d651f79971bd
Oleksii <Kolisnyk@inbox.ru>
2016-04-20 11:53:04 +0300

    New tutorial_ v.1.1

---
+++ book_spec.rb
@@ -0,0 +1,13 @@
+require 'rspec'
+require_relative 'book.rb'
+require_relative 'library.sqlite'
+require_relative 'Draftlibrar.sqlite'
+
+
+describe 'Book' do
+
+  it 'should do something' do
+
+    true.should == false
+  end
+end
